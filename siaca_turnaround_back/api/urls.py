1348e6e08fccc62d6e5e7ace29b10494427c30bb
mari1811 <maririos181@gmail.com>
2023-06-07 12:12:53 -0400

    Consultas

--- siaca_turnaround_back/api/urls.py
+++ siaca_turnaround_back/api/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
-from .views import MaquinariaView
+from .views import MaquinariaView, CodigoDemoraView, TurnaroundView, UsuarioView, AerolineaView, PlantillaView, VueloView
 
 urlpatterns=[
     path('maquinaria/',MaquinariaView.as_view(), name='maquinarias'),
-    path('maquinaria/<int:id>',MaquinariaView.as_view(), name='maquina')
+    path('maquinaria/<int:id>',MaquinariaView.as_view(), name='maquina'),
+
+    path('codigodemora/',CodigoDemoraView.as_view(), name='codigodemora'),
+
+    path('turnaround/',TurnaroundView.as_view(), name='turnarounds'),
+
+    path('usuario/',UsuarioView.as_view(), name='usuarios'),
+
+    path('aerolinea/',AerolineaView.as_view(), name='aerolineas'),
+
+    path('plantilla/',PlantillaView.as_view(), name='plantillas'),
+
+    path('vuelo/',VueloView.as_view(), name='vuelos')
 ]
--- siaca_turnaround_back/api/views.py
+++ siaca_turnaround_back/api/views.py
@@ -3,7 +3,7 @@ from django.utils.decorators import method_decorator
 from django.shortcuts import render
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
-from .models import maquinaria
+from .models import maquinaria, codigos_demora, turnaround, usuario, aerolinea, plantilla, vuelo
 import json
 
 # Create your views here.
@@ -53,3 +53,99 @@ class MaquinariaView(View):
         else:
             datos={'mensaje':'No hay maquinarias'}
         return JsonResponse(datos)
+    
+
+class CodigoDemoraView(View):
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get(self, request):
+        codigo = list(codigos_demora.objects.values())
+        if len(codigo)>0:
+            datos={'mensaje':'Success','codigoss':codigo}
+        else:
+            datos={'mensaje':'No hay codigos'}
+        return JsonResponse(datos)
+    
+
+class TurnaroundView(View):
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get(self, request):
+        turnarounds = list(turnaround.objects.values())
+        if len(turnarounds)>0:
+            datos={'mensaje':'Success','turnarounds':turnarounds}
+        else:
+            datos={'mensaje':'No hay turnarounds'}
+        return JsonResponse(datos)
+    
+class UsuarioView(View):
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get(self, request):
+        usuarios = list(usuario.objects.values())
+        if len(usuarios)>0:
+            datos={'mensaje':'Success','usuarios':usuarios}
+        else:
+            datos={'mensaje':'No hay usuarios'}
+        return JsonResponse(datos)
+    
+    def post(self, request):
+        jsondata = json.loads(request.body)
+        usuario.objects.create(cedula=jsondata['cedula'], cargo=jsondata['cargo'], departameto=jsondata['departamento'], correo=jsondata['correo'], telefono=jsondata['turno'] ,contrasena=jsondata['contrasena'], estado=jsondata['estado'] ,imagen=jsondata['imagen'])
+        datos={'mensaje':'Success'}
+        return JsonResponse(datos)
+    
+
+class AerolineaView(View):
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get(self, request):
+        aerolineas = list(aerolinea.objects.values())
+        if len(aerolineas)>0:
+            datos={'mensaje':'Success','aerolineas':aerolineas}
+        else:
+            datos={'mensaje':'No hay aerolineas'}
+        return JsonResponse(datos)
+    
+
+class PlantillaView(View):
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get(self, request):
+        plantillas = list(plantilla.objects.values())
+        if len(plantillas)>0:
+            datos={'mensaje':'Success','plantillas':plantillas}
+        else:
+            datos={'mensaje':'No hay plantillas'}
+        return JsonResponse(datos)
+    
+
+class VueloView(View):
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get(self, request):
+        vuelos = list(vuelo.objects.values())
+        if len(vuelos)>0:
+            datos={'mensaje':'Success','vuelos':vuelos}
+        else:
+            datos={'mensaje':'No hay vuelos'}
+        return JsonResponse(datos)
+    
