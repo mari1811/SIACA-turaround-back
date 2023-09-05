b8d0850d79162de36fc9567b942cb3c7cb537819
mari1811 <maririos181@gmail.com>
2023-09-05 13:46:19 -0400

    Arreglo datos turnaround

--- siaca_turnaround_back/api/turnarounds/api/serializer.py
+++ siaca_turnaround_back/api/turnarounds/api/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from api.models import turnaround, usuario_turnaround, maquinaria_turnaround, codigos_demora, vuelo, maquinaria
+from api.models import turnaround, usuario_turnaround, maquinaria_turnaround, codigos_demora, vuelo, maquinaria, ciudades, ciudades_destino, ciudades_salida, aerolinea, tipo_vuelo, plantilla
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 
@@ -34,14 +34,53 @@ class VueloSerializer(serializers.ModelSerializer):
         model = vuelo
         fields = ('numero_vuelo',)   
 
+class CiudadDetalleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ciudades
+        fields = '__all__'
+
+class CiudadDestinoDetalleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ciudades_destino
+        fields = '__all__'
+
+class CiudadSalidaDetalleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ciudades_salida
+        fields = '__all__'
+
+class AerolineaDetalleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = aerolinea
+        fields = '__all__'
+
+class TipoVueloDetalleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = tipo_vuelo
+        fields = '__all__'
+
+class PlantillaDetalleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = plantilla
+        fields = '__all__'
+
 class VueloDetalleSerializer(serializers.ModelSerializer):
+    lugar_salida = CiudadSalidaDetalleSerializer()
+    lugar_destino = CiudadDestinoDetalleSerializer()
+    stn = CiudadDetalleSerializer()
+    fk_aerolinea = AerolineaDetalleSerializer()
+    tipo_vuelo = TipoVueloDetalleSerializer()
+    fk_plantilla = PlantillaDetalleSerializer()
+
     class Meta:
         model = vuelo
         fields = '__all__' 
 
+
 class TurnaroundFechaSerializer(serializers.ModelSerializer):
     fk_codigos_demora = CodigosDemoraSerializer()
     fk_vuelo = VueloDetalleSerializer()
+    
     class Meta:
         model = turnaround
         fields = '__all__'
