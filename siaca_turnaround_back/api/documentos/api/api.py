d7f679f7dde3950e029a74e440a598c8653364cd
mari1811 <maririos181@gmail.com>
2024-04-16 22:13:15 -0400

    Arreglo correo

--- siaca_turnaround_back/api/documentos/api/api.py
+++ siaca_turnaround_back/api/documentos/api/api.py
@@ -168,58 +168,63 @@ class TurnaroundCorreo(APIView):
             datos = turnaround.objects.filter(id = pk).first()
             documento_serializer = TurnaoundSerializer(datos)
 
-            # Format the data as specified
-            formatted_data = ""
-            formatted_data = "<table style='width:100%; border: 1px solid black; border-collapse: collapse;'>\n"
-            formatted_data += "  <tr style='border: 1px solid black;'><th style='border: 1px solid black; padding: 5px;'>Task</th><th style='border: 1px solid black; padding: 5px;'>Time or Comment</th></tr>\n"
 
-            for hora in horas:
-                formatted_data += f"  <tr style='border: 1px solid black;'><td style='border: 1px solid black; padding: 5px;'>{hora['fk_subtarea__titulo']}</td><td style='border: 1px solid black; padding: 5px;'>{hora['hora_inicio']}</td></tr>\n"
+            if datos:
+                # Format the data as specified
+                formatted_data = ""
+                formatted_data = "<table style='width:100%; border: 1px solid black; border-collapse: collapse;'>\n"
+                formatted_data += "  <tr style='border: 1px solid black;'><th style='border: 1px solid black; padding: 5px;'>Task</th><th style='border: 1px solid black; padding: 5px;'>Time or Comment</th></tr>\n"
 
-            for hora_inicio_fin in horas_inicio_fin:
-                formatted_data += f"  <tr style='border: 1px solid black;'><td style='border: 1px solid black; padding: 5px;'>{hora_inicio_fin['fk_subtarea__titulo']}</td><td style='border: 1px solid black; padding: 5px;'>{hora_inicio_fin['hora_inicio']} - {hora_inicio_fin['hora_fin']}</td></tr>\n"
+                for hora in horas:
+                    formatted_data += f"  <tr style='border: 1px solid black;'><td style='border: 1px solid black; padding: 5px;'>{hora['fk_subtarea__titulo']}</td><td style='border: 1px solid black; padding: 5px;'>{hora['hora_inicio']}</td></tr>\n"
 
-            for comentario in comentarios:
-                formatted_data += f"  <tr style='border: 1px solid black;'><td style='border: 1px solid black; padding: 5px;'>{comentario['fk_subtarea__titulo']}</td><td style='border: 1px solid black; padding: 5px;'>{comentario['comentario']}</td></tr>\n"
+                for hora_inicio_fin in horas_inicio_fin:
+                    formatted_data += f"  <tr style='border: 1px solid black;'><td style='border: 1px solid black; padding: 5px;'>{hora_inicio_fin['fk_subtarea__titulo']}</td><td style='border: 1px solid black; padding: 5px;'>{hora_inicio_fin['hora_inicio']} - {hora_inicio_fin['hora_fin']}</td></tr>\n"
 
-            formatted_data += "</table>\n"
+                for comentario in comentarios:
+                    formatted_data += f"  <tr style='border: 1px solid black;'><td style='border: 1px solid black; padding: 5px;'>{comentario['fk_subtarea__titulo']}</td><td style='border: 1px solid black; padding: 5px;'>{comentario['comentario']}</td></tr>\n"
 
-            # Send the email with the formatted data
-            subject = 'Turnaround Information'
-            message = f'<html><body>\n' 
-            message+= f'<p>Los servicios de asistencia en tierra realizados por Siaca han sido completados exitosamente, el sistema generó el siguiente reporte: </p>\n'
-            message+= f'<p>Datos del vuelo: </p>\n'
-            message += f'<table style="width:100%; border: 0.5px solid black; border-collapse: collapse; border-radius: 5px;">\n'
+                formatted_data += "</table>\n"
 
-            message += f'  <tr style="border: 0.5px solid black;"><th style="border: 0.5px solid black; padding: 5px;">Codigo de Demora:</th><td style="border: 0.5px solid black; padding: 5px;"><strong>Identificador:</strong> {documento_serializer.data["fk_codigos_demora"]["identificador"]} - <strong>Alpha:</strong> {documento_serializer.data["fk_codigos_demora"]["alpha"]}</td><td style="border: 0.5px solid black; padding: 5px;"><strong>Descripción:</strong> {documento_serializer.data["fk_codigos_demora"]["descripcion"]}</td><td style="border: 0.5px solid black; padding: 5px;"><strong>Accountable:</strong> {documento_serializer.data["fk_codigos_demora"]["accountable"]}</td></tr>\n'
+                # Send the email with the formatted data
+                subject = 'Turnaround Information'
+                message = f'<html><body>\n' 
+                message+= f'<p>Los servicios de asistencia en tierra realizados por Siaca han sido completados exitosamente, el sistema generó el siguiente reporte: </p>\n'
+                message+= f'<p>Datos del vuelo: </p>\n'
+                message += f'<table style="width:100%; border: 0.5px solid black; border-collapse: collapse; border-radius: 5px;">\n'
 
-            message += f'  <tr style="border: 0.5px solid black;"><th style="border: 0.5px solid black; padding: 5px;">Date and Start Time:</th><td style="border: 0.5px solid black; padding: 5px;">{documento_serializer.data["fecha_inicio"]} - {documento_serializer.data["hora_inicio"]}</td><th style="border: 0.5px solid black; padding: 5px;">Service Type:</th><td style="border: 0.5px solid black; padding: 5px;">{documento_serializer.data["fk_vuelo"]["tipo_servicio"]["nombre"]}</td></tr>\n'
+                message += f'  <tr style="border: 0.5px solid black;"><th style="border: 0.5px solid black; padding: 5px;">Codigo de Demora:</th><td style="border: 0.5px solid black; padding: 5px;"><strong>Identificador:</strong> {documento_serializer.data["fk_codigos_demora"]["identificador"]} - <strong>Alpha:</strong> {documento_serializer.data["fk_codigos_demora"]["alpha"]}</td><td style="border: 0.5px solid black; padding: 5px;"><strong>Descripción:</strong> {documento_serializer.data["fk_codigos_demora"]["descripcion"]}</td><td style="border: 0.5px solid black; padding: 5px;"><strong>Accountable:</strong> {documento_serializer.data["fk_codigos_demora"]["accountable"]}</td></tr>\n'
 
-            message += f'  <tr style="border: 0.5px solid black;"><th style="border: 0.5px solid black; padding: 5px;">STN:</th><td style="border: 0.5px solid black; padding: 5px;">{documento_serializer.data["fk_vuelo"]["stn_id"]["codigo"]}</td><th style="border: 0.5px solid black; padding: 5px;">Charges Payable By:</th><td style="border: 0.5px solid black; padding: 5px;">{documento_serializer.data["fk_vuelo"]["ente_pagador"]}</td></tr>\n'
+                message += f'  <tr style="border: 0.5px solid black;"><th style="border: 0.5px solid black; padding: 5px;">Date and Start Time:</th><td style="border: 0.5px solid black; padding: 5px;">{documento_serializer.data["fecha_inicio"]} - {documento_serializer.data["hora_inicio"]}</td><th style="border: 0.5px solid black; padding: 5px;">Service Type:</th><td style="border: 0.5px solid black; padding: 5px;">{documento_serializer.data["fk_vuelo"]["tipo_servicio"]["nombre"]}</td></tr>\n'
 
-            message += f'  <tr style="border: 0.5px solid black;"><th style="border: 0.5px solid black; padding: 5px;">Flight No.:</th><td style="border: 0.5px solid black; padding: 5px;">{documento_serializer.data["fk_vuelo"]["numero_vuelo"]}</td><th style="border: 0.5px solid black; padding: 5px;">Aircraft Reg:</th><td style="border: 0.5px solid black; padding: 5px;">{documento_serializer.data["fk_vuelo"]["ac_reg"]}</td></tr>\n'
+                message += f'  <tr style="border: 0.5px solid black;"><th style="border: 0.5px solid black; padding: 5px;">STN:</th><td style="border: 0.5px solid black; padding: 5px;">{documento_serializer.data["fk_vuelo"]["stn_id"]["codigo"]}</td><th style="border: 0.5px solid black; padding: 5px;">Charges Payable By:</th><td style="border: 0.5px solid black; padding: 5px;">{documento_serializer.data["fk_vuelo"]["ente_pagador"]}</td></tr>\n'
 
-            message += f'  <tr style="border: 0.5px solid black;"><th style="border: 0.5px solid black; padding: 5px;">Routing:</th><td style="border: 0.5px solid black; padding: 5px;">{documento_serializer.data["fk_vuelo"]["lugar_salida"]["codigo"]}/{documento_serializer.data["fk_vuelo"]["lugar_destino"]["codigo"]}</td><th style="border: 0.5px solid black; padding: 5px;">Flight Type:</th><td style="border: 0.5px solid black; padding: 5px;">{documento_serializer.data["fk_vuelo"]["tipo_vuelo"]["nombre"]}</td></tr>\n'
+                message += f'  <tr style="border: 0.5px solid black;"><th style="border: 0.5px solid black; padding: 5px;">Flight No.:</th><td style="border: 0.5px solid black; padding: 5px;">{documento_serializer.data["fk_vuelo"]["numero_vuelo"]}</td><th style="border: 0.5px solid black; padding: 5px;">Aircraft Reg:</th><td style="border: 0.5px solid black; padding: 5px;">{documento_serializer.data["fk_vuelo"]["ac_reg"]}</td></tr>\n'
 
-            message += f'  <tr style="border: 0.5px solid black;"><th style="border: 0.5px solid black; padding: 5px;">Gate/Gate:</th><td style="border: 0.5px solid black; padding: 5px;">{documento_serializer.data["fk_vuelo"]["gate"]}</td><th style="border: 0.5px solid black; padding: 5px;">AircraftType:</th><td style="border: 0.5px solid black; padding: 5px;">{documento_serializer.data["fk_vuelo"]["ac_type"]}</td></tr>\n'
+                message += f'  <tr style="border: 0.5px solid black;"><th style="border: 0.5px solid black; padding: 5px;">Routing:</th><td style="border: 0.5px solid black; padding: 5px;">{documento_serializer.data["fk_vuelo"]["lugar_salida"]["codigo"]}/{documento_serializer.data["fk_vuelo"]["lugar_destino"]["codigo"]}</td><th style="border: 0.5px solid black; padding: 5px;">Flight Type:</th><td style="border: 0.5px solid black; padding: 5px;">{documento_serializer.data["fk_vuelo"]["tipo_vuelo"]["nombre"]}</td></tr>\n'
 
-            message += f'  <tr style="border: 0.5px solid black;"><th style="border: 0.5px solid black; padding: 5px;">ETA:</th><td style="border: 0.5px solid black; padding: 5px;">{documento_serializer.data["fk_vuelo"]["ETA"]} - {documento_serializer.data["fk_vuelo"]["ETA_fecha"]}</td><th style="border: 0.5px solid black; padding: 5px;">ETD:</th><td style="border: 0.5px solid black; padding: 5px;">{documento_serializer.data["fk_vuelo"]["ETD"]} - {documento_serializer.data["fk_vuelo"]["ETD_fecha"]}</td></tr>\n'
+                message += f'  <tr style="border: 0.5px solid black;"><th style="border: 0.5px solid black; padding: 5px;">Gate/Gate:</th><td style="border: 0.5px solid black; padding: 5px;">{documento_serializer.data["fk_vuelo"]["gate"]}</td><th style="border: 0.5px solid black; padding: 5px;">AircraftType:</th><td style="border: 0.5px solid black; padding: 5px;">{documento_serializer.data["fk_vuelo"]["ac_type"]}</td></tr>\n'
 
-            message += f'</table>\n'
-            message += f'<p>Tareas realizadas:</p>\n'
-            message += f'{formatted_data}</body></html>\n'
+                message += f'  <tr style="border: 0.5px solid black;"><th style="border: 0.5px solid black; padding: 5px;">ETA:</th><td style="border: 0.5px solid black; padding: 5px;">{documento_serializer.data["fk_vuelo"]["ETA"]} - {documento_serializer.data["fk_vuelo"]["ETA_fecha"]}</td><th style="border: 0.5px solid black; padding: 5px;">ETD:</th><td style="border: 0.5px solid black; padding: 5px;">{documento_serializer.data["fk_vuelo"]["ETD"]} - {documento_serializer.data["fk_vuelo"]["ETD_fecha"]}</td></tr>\n'
 
-            message += f'<p style=color: #D5D8DC >*Esto es un reporte preliminar, el documento oficial será enviado posteriormente por Siaca*</p>\n'
+                message += f'</table>\n'
+                message += f'<p>Tareas realizadas:</p>\n'
+                message += f'{formatted_data}</body></html>\n'
 
-            from_email = settings.EMAIL_HOST_USER
-            to_email = [documento_serializer.data["fk_vuelo"]["fk_aerolinea"]["correo"]]
+                message += f'<p style=color: #D5D8DC >*Esto es un reporte preliminar, el documento oficial será enviado posteriormente por Siaca*</p>\n'
 
-            email = EmailMultiAlternatives(subject, message, from_email, to_email)
-            email.attach_alternative(message, "text/html")
-            email.send()
+                from_email = settings.EMAIL_HOST_USER
+                to_email = [documento_serializer.data["fk_vuelo"]["fk_aerolinea"]["correo"]]
 
-            # Return the data as before
-            return Response({"mensaje enviado"}, status=status.HTTP_200_OK)
+                email = EmailMultiAlternatives(subject, message, from_email, to_email)
+                email.attach_alternative(message, "text/html")
+                email.send()
+
+                # Return the data as before
+                return Response({"mensaje enviado"}, status=status.HTTP_200_OK)
+            else:
+                 return Response({"mensaje no enviado"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'mensaje':'Token no válido'}, status=status.HTTP_400_BAD_REQUEST)
         
 
 class UpdateHora(generics.RetrieveUpdateDestroyAPIView):
